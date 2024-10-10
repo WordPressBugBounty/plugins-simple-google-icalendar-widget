93967226ab0082885ecbd48cae384ecb37c4256e
Sean Murphy <sean@iamseanmurphy.com>
2024-10-10 21:06:45 +0000

    2.4.4

---
+++ simple-google-icalendar-widget/js/simple-ical-block-fetch.asset.php
@@ -0,0 +1,5 @@
+<?php 
+/*
+ * version 2.4.4
+ */
+return ['dependencies' => ['wp-api-fetch'],  'version' => '2.4.4-' . filemtime( plugin_dir_path( __FILE__ ) . 'simple-ical-block-fetch.js' ) ];
---
+++ simple-google-icalendar-widget/js/simple-ical-block-view.asset.php
@@ -0,0 +1,5 @@
+<?php 
+/*
+ * version 2.4.4
+ */
+return ['dependencies' => [],  'version' => '2.4.4-' . filemtime( plugin_dir_path( __FILE__ ) . 'simple-ical-block-view.js' ) ];
