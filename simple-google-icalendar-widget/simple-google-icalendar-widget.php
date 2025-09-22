d0f9179dac9e41736fbb6ee1bf42ccb6fba0e404
Sean Murphy <sean@iamseanmurphy.com>
2025-09-22 09:33:59 +0000

    2.7.1

--- simple-google-icalendar-widget/simple-google-icalendar-widget.php
+++ simple-google-icalendar-widget/simple-google-icalendar-widget.php
@@ -4,7 +4,7 @@
  Description: Widget that displays events from a public google calendar or iCal file
  Plugin URI: https://github.com/bramwaas/wordpress-plugin-wsa-simple-google-calendar-widget
  Author: Bram Waasdorp
- Version: 2.7.0
+ Version: 2.7.1
  License: GPLv2
  Tested up to: 6.8
  Requires at least: 5.3
@@ -22,9 +22,10 @@
  *      wp-widget in array with sibid as index so that the attributes are available for REST call.
  *   bw 20240509 v2.4.1 added defaults to all used keys of $args to solve issue 'PHP warnings' of johansam on support forum. Undefined array key “classname” in .../simple-google-icalendar-widget.php on line 170
  *   bw 20240727 v2.4.4 simplified defaulting args and improved code around that for the widget output
- *   bw 20241028 v2.5.0 Add support for categories    Tested with 6.7-RC and 5.9.5. 
+ *   bw 20241028 v2.5.0 Add support for categories    Tested with 6.7-RC and 5.9.5.
  *   bw 20250112 v2.6.0 plugin check, Using simple classloader and PSR-4 name conventions. Moved  SimpleicalWidget class to separate file.
  *   bw 20250219 v2.6.1 use bootstrap collapse script if desired
+ *   bw 20250922 v2.7.1 Additional selection on Namespace in Classloader
  */
 /*
  Simple Google Calendar Outlook Events Widget
@@ -74,44 +75,45 @@ if ($options['simpleical_add_collapse_code']){
 }
 if ($options['simpleical_add_collapse_code_admin']){
     add_action( 'enqueue_block_assets', __NAMESPACE__ .'\enqueue_bs_block_assets' );}/**
- * Register our simple_ical_settings_init to the admin_init action hook.
- * Register our simple_ical_options_page and simple_ical_info_page to the admin_menu action hook.
- */
-add_action( 'admin_init', [$ical_admin, 'simple_ical_settings_init'] );
-add_action('admin_menu',[$ical_admin, 'simple_ical_options_page']);
-add_action('admin_menu',array ($ical_admin, 'simple_ical_info_page'));
-
-/**
- * enqueue scripts for use in client REST view
- * for v 6.3 up args array strategy = defer, else in_footer = that array is casted to boolean true. 
- */
-function enqueue_view_script()
-{
-    wp_enqueue_script('simplegoogleicalenderwidget-simple-ical-block-view-script', plugins_url('/js/simple-ical-block-view.js', __FILE__), [], '2.6.1-' . filemtime(plugin_dir_path(__FILE__) . 'js/simple-ical-block-view.js'), 
-        ['strategy' => 'defer' ]);
-    wp_add_inline_script('simplegoogleicalenderwidget-simple-ical-block-view-script', '(window.simpleIcalBlock=window.simpleIcalBlock || {}).restRoot = "' . get_rest_url() . '"', 'before');
-}
+    * Register our simple_ical_settings_init to the admin_init action hook.
+    * Register our simple_ical_options_page and simple_ical_info_page to the admin_menu action hook.
+    */
+    add_action( 'admin_init', [$ical_admin, 'simple_ical_settings_init'] );
+    add_action('admin_menu',[$ical_admin, 'simple_ical_options_page']);
+    add_action('admin_menu',array ($ical_admin, 'simple_ical_info_page'));
+    
     /**
- * enqueue bootstrap scripts and css for collapse (in footer to load it only when 'add_collapse_code' is true)
- * for v 6.3 up args array strategy = defer, else in_footer = that array is casted to boolean true.
- */
-function enqueue_bs_scripts()
-{
-    wp_enqueue_script('simplegoogleicalenderwidget-collapse-bundle-script', plugins_url('/vendor/bs/js/collapse.bundle.js', __FILE__),
-        [],
-        '5.3.3-' . filemtime(plugin_dir_path(__FILE__) . 'vendor/bs/js/collapse.bundle.js'),
-        ['strategy' => 'defer' ]);
-    wp_enqueue_style('simplegoogleicalenderwidget-collapse-style', plugins_url('/vendor/bs/css/collapse.css', __FILE__),
-        [],
-        '5.3.3-' . filemtime(plugin_dir_path(__FILE__) . 'vendor/bs/css/collapse.css'),
-        'all');
-}
-/**
- * Enqueue block content assets but only in the Editor.
- */
-function enqueue_bs_block_assets() {
-    if ( is_admin() ) {
-        enqueue_bs_scripts();
+     * enqueue scripts for use in client REST view
+     * for v 6.3 up args array strategy = defer, else in_footer = that array is casted to boolean true.
+     */
+    function enqueue_view_script()
+    {
+        wp_enqueue_script('simplegoogleicalenderwidget-simple-ical-block-view-script', plugins_url('/js/simple-ical-block-view.js', __FILE__), [], '2.6.1-' . filemtime(plugin_dir_path(__FILE__) . 'js/simple-ical-block-view.js'),
+            ['strategy' => 'defer' ]);
+        wp_add_inline_script('simplegoogleicalenderwidget-simple-ical-block-view-script', '(window.simpleIcalBlock=window.simpleIcalBlock || {}).restRoot = "' . get_rest_url() . '"', 'before');
     }
-}
+    /**
+     * enqueue bootstrap scripts and css for collapse (in footer to load it only when 'add_collapse_code' is true)
+     * for v 6.3 up args array strategy = defer, else in_footer = that array is casted to boolean true.
+     */
+    function enqueue_bs_scripts()
+    {
+        wp_enqueue_script('simplegoogleicalenderwidget-collapse-bundle-script', plugins_url('/vendor/bs/js/collapse.bundle.js', __FILE__),
+            [],
+            '5.3.3-' . filemtime(plugin_dir_path(__FILE__) . 'vendor/bs/js/collapse.bundle.js'),
+            ['strategy' => 'defer' ]);
+        wp_enqueue_style('simplegoogleicalenderwidget-collapse-style', plugins_url('/vendor/bs/css/collapse.css', __FILE__),
+            [],
+            '5.3.3-' . filemtime(plugin_dir_path(__FILE__) . 'vendor/bs/css/collapse.css'),
+            'all');
+    }
+    /**
+     * Enqueue block content assets but only in the Editor.
+     */
+    function enqueue_bs_block_assets() {
+        if ( is_admin() ) {
+            enqueue_bs_scripts();
+        }
+    }
     add_action ('widgets_init', array (__NAMESPACE__ .'\SimpleicalHelper','simple_ical_widget')  );
+    
